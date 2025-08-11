a6955e7f7eddbe92e71dab2635e8b35a41ef5ccf
cariangela <b2029camgabriel@pshs.edu.ph>
2025-08-11 11:49:35 +0800

    1st nongraded

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let age = 14; 
 let maxAge = 98;
-let numPerDay = "2 packs of seaweed";
+let numPerDay = 2;
 let totalRequired = (numPerDay * 365) * (maxAge - age);
 
-document.write( "You will need " + totalRequired + " to last you until the ripe old age of " + maxAge ".");
+document.write( "You will need " + totalRequired + " packs of seaweed to last you until the ripe old age of " + maxAge + ".");
 
